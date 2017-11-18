c0dc23c322ddb1ea6c52f22f6ca9f737575fd196
Artur Araújo Santos Ribeiro <arturrj@hotmail.com>
2017-11-18 08:39:14 -0200

    Add project files.

---
+++ src/Scorponok.Gateway.Pagamento.Unit.Test/Commands/CancelamentoCommandHandlerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Scorponok.Gateway.Pagamento.Unit.Test.Commands
+{
+    public class CancelamentoCommandHandlerTests
+    {
+        [Fact]
+        public void Realiza_um_cancelamento()
+        {
+            #region Arrange
+            #endregion
+
+            #region Stub's
+            #endregion
+
+            #region Act
+            #endregion
+
+            #region Assert's 
+            #endregion
+
+        }
+    }
+}
